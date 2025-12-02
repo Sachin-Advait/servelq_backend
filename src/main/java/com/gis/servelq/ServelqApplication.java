c4f16096911a6c44431a4ad9c69a96656fd2c87d
dipesh shrestha <dipeshshrestha876@gmail.com>
2025-12-02 17:05:16 +0400

    added the auto create table.

--- src/main/java/com/gis/servelq/ServelqApplication.java
+++ src/main/java/com/gis/servelq/ServelqApplication.java
@@ -4,17 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.gis.servelq.models")
+@EnableJpaRepositories(basePackages = "com.gis.servelq.repository")
 public class ServelqApplication implements ApplicationListener<WebServerInitializedEvent> {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServelqApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServelqApplication.class, args);
+    }
 
-	@Override
-	public void onApplicationEvent(WebServerInitializedEvent event) {
-		int port = event.getWebServer().getPort();
-		System.out.println("Server Started At PORT: " + port);
-	}
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        int port = event.getWebServer().getPort();
+        System.out.println("Server Started At PORT: " + port);
+    }
 }
