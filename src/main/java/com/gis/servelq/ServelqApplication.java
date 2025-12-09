1bf870f9d7617036ae82026a0959349251d3b08c
Sachin Advait <sachinadvaita@gmail.com>
2025-12-09 20:18:05 +0530

    Fixed the twilio token GitHub error.

--- src/main/java/com/gis/servelq/ServelqApplication.java
+++ src/main/java/com/gis/servelq/ServelqApplication.java
@@ -1,10 +1,11 @@
 package com.gis.servelq;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -13,7 +14,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class ServelqApplication implements ApplicationListener<WebServerInitializedEvent> {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        // Set system properties for Spring Boot
+        setSystemProperty("TWILIO_SID", dotenv);
+        setSystemProperty("TWILIO_TOKEN", dotenv);
+        setSystemProperty("TWILIO_FROM", dotenv);
+
         SpringApplication.run(ServelqApplication.class, args);
+    }
+
+    private static void setSystemProperty(String key, Dotenv dotenv) {
+        String value = dotenv.get(key);
+        if (value != null) {
+            System.setProperty(key, value);
+        }
     }
 
     @Override
--- src/main/java/com/gis/servelq/controllers/WhatsAppController.java
+++ src/main/java/com/gis/servelq/controllers/WhatsAppController.java
@@ -1,7 +1,7 @@
 package com.gis.servelq.controllers;
 
 import com.gis.servelq.services.WhatsAppService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/serveiq/api/whatsapp")
+@AllArgsConstructor
 public class WhatsAppController {
 
-    @Autowired
-    private WhatsAppService service;
+    final private WhatsAppService service;
 
     @GetMapping("/send")
     public String send(@RequestParam String to, @RequestParam String msg) {
